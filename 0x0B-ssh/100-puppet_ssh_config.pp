d251a5dcd5343daddf464e97ef9dcc3d217562fb
Sammking120 <sammking120@gmail.com>
2025-06-29 14:26:20 +0300

    Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,12 @@
 # Puppet script to create ssh config file
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+}
 
-file { '/home/sammking/.ssh/config':
-    ensure  => file,
-    owner   => 'sammking',
-    group   => 'sammking',
-    mode    => '0600',
-    content => "Host myserver
-    HostName 44.204.0.219
-    User ubuntu
-  I dentityFile ~/.ssh/school
-    PasswordAuthentication no
-",
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
 }
