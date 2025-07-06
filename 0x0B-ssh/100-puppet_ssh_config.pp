03e5f8cea3e0e482924440bcb6df804e7fb233eb
Sammking120 <sammking120@gmail.com>
2025-07-06 20:46:03 +0300

    Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,15 @@
 # Puppet script to create ssh config file
 
-file_line { 'Turn off passwd auth':
-  ensure => 'present',
-  path   => '/etc/ssh/ssh_config',
-  line   => '    PasswordAuthentication no',
+file_line { 'Turn off passwd':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => '     PasswordAuthentication no',
+  replace => true,
 }
 
-file_line { 'Declare identity file':
-  ensure => 'present',
-  path   => '/etc/ssh/ssh_config',
-  line   => '    IdentityFile ~/.ssh/school',
+file_line { 'identity change':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => '     IdentityFile ~/.ssh/school',
+  replace => true,
 }
